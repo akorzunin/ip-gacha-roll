88b061ba7d6a1b7011a5a5de53aea4e89910c16c
akorzunin <akorzunin123@gmail.com>
2025-06-21 18:45:45 +0300

    feat: moved nat check to shared module

--- src-tauri/src/check_nat.rs
+++ src-tauri/src/check_nat.rs
@@ -1,7 +1,6 @@
-use std::{net::Ipv4Addr, time::Duration};
-
-use reqwest::{header::USER_AGENT, Client};
 use serde::{Deserialize, Serialize};
+
+use crate::shared::net_utils::{get_ip, ping_ip_tcp};
 
 #[derive(Serialize, Deserialize)]
 pub struct CheckNatRes {
@@ -37,88 +36,4 @@ pub async fn check_nat() -> Result<CheckNatRes, CheckNatErr> {
         ip: ip.to_string(),
         nat,
     })
-}
-
-#[derive(Serialize, Deserialize)]
-struct IpRes {
-    ip: String,
-}
-
-pub async fn get_ip() -> Result<Ipv4Addr, anyhow::Error> {
-    let client = Client::new();
-    let res = client
-        .get("https://ifconfig.me")
-        .header(USER_AGENT, "curl")
-        .send()
-        .await?;
-    let ip_res = &res.text().await?;
-    let ip = ip_res.parse::<Ipv4Addr>()?;
-    Ok(ip)
-}
-
-#[allow(dead_code)]
-pub async fn ping_ip(ip: Ipv4Addr, timeout: Option<Duration>) -> Result<bool, anyhow::Error> {
-    let timeout = timeout.unwrap_or(Duration::from_secs(10));
-    let ping_future = surge_ping::ping(ip.to_string().parse()?, &[0; 32]);
-    match tokio::time::timeout(timeout, ping_future).await {
-        Ok(_) => Ok(true),
-        Err(_) => Ok(false),
-    }
-}
-pub async fn ping_ip_tcp(ip: Ipv4Addr, timeout: Option<Duration>) -> Result<bool, anyhow::Error> {
-    let timeout = timeout.unwrap_or(Duration::from_secs(10));
-    let ping_future = async {
-        reqwest::Client::new()
-            .get(format!("http://{}:80", ip))
-            .send()
-            .await
-    };
-    match tokio::time::timeout(timeout, ping_future).await {
-        Ok(res) => match res {
-            Ok(res) => match res.status() {
-                reqwest::StatusCode::PERMANENT_REDIRECT => Ok(true),
-                _ => Ok(false),
-            },
-            Err(_) => Ok(false),
-        },
-        Err(_) => Ok(false),
-    }
-}
-
-#[tokio::test]
-async fn test_get_ip() {
-    let ip = get_ip().await.unwrap();
-    println!("ip: {}", ip.to_string());
-    assert_eq!(ip.is_loopback(), false);
-}
-
-#[tokio::test]
-async fn test_ping_ip() {
-    let ip = get_ip().await.unwrap();
-    let nat = ping_ip(ip, None).await.unwrap();
-    assert_eq!(nat, true);
-}
-
-#[tokio::test]
-async fn test_ping_ip_tcp() {
-    let ip = get_ip().await.unwrap();
-    let nat = ping_ip_tcp(ip, None).await.unwrap();
-    assert_eq!(nat, true);
-}
-
-#[tokio::test]
-async fn test_ping_ip_loopback() {
-    let nat = ping_ip(Ipv4Addr::new(127, 0, 0, 1), None).await.unwrap();
-    assert_eq!(nat, true);
-}
-
-#[tokio::test]
-async fn test_ping_ip_not_nat() {
-    let nat = ping_ip(
-        Ipv4Addr::new(198, 17, 255, 255),
-        Some(Duration::from_secs(1)),
-    )
-    .await
-    .unwrap();
-    assert_eq!(nat, false);
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ mod check_nat;
 mod keen_client;
 mod migrations;
 mod settings;
+mod shared;
 
 struct AppState<'a> {
     keen_client: &'a reqwest::blocking::Client,
